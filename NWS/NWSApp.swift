4388950c0cfb4aa77d2e9ad195e9bc076fe59780
DVP <dvp@TechAcademy01s-MacBook-Pro.local>
2024-02-19 11:07:07 +0300

    Initial Commit

---
+++ NWS/NWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NWSApp.swift
+//  NWS
+//
+//  Created by DVP on 19/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct NWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
